cdf5d2305907803482d626e3634e128aa0f854ea
Ashley Peart <peart.ashley@gmail>
2022-02-25 15:27:57 -0500

    initial commit

---
+++ palindrome.py
@@ -0,0 +1,34 @@
+'''
+s = input ("enter a string:")
+
+revstr=s[::-1]
+
+if (revstr ==s):
+    print ("yes a palindrome")
+else:
+    print("not a palindrome")
+'''
+
+'''
+s = input ("enter a string:")
+
+revstr = (s[::-1])
+
+if (s ==revstr):
+    print ("hey")
+else:
+    print("yo")
+
+'''
+
+
+
+s = input ("enter a string")
+
+revstr = s[::-1]
+
+if (revstr ==s):
+    print("yes , it's a  palindrome")
+else:
+    print("No it's not a palindrome")
+
